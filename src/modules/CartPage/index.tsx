85ac230db94c72b34d662a5073466390428aa391
chandraapriana <aprianachandra@gmail.com>
2023-09-05 20:42:47 +0700

    feat: create header component

--- src/modules/CartPage/index.tsx
+++ src/modules/CartPage/index.tsx
@@ -1,9 +1,13 @@
+import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 
 const CartPage = () => {
   return (
     <main className="flex min-h-screen">
       <Sidebar />
+      <div className="w-full h-14">
+        <Header title="Cart" />
+      </div>
     </main>
   );
 };
