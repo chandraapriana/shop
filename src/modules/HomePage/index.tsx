8c8a5cf4193f8bbdbde935e514e8c0bc3ffea3b8
chandraapriana <aprianachandra@gmail.com>
2023-09-05 19:37:40 +0700

    feat: create chart component

--- src/modules/HomePage/index.tsx
+++ src/modules/HomePage/index.tsx
@@ -1,9 +1,9 @@
 import Sidebar from "@/components/Sidebar"
 import BarChart from "./components/BarChart"
-
+import '@/styles/globals.css'
 const HomePage = () => {
   return (
-    <main className="flex h-screen flex-row" >
+    <main className="flex h-screen px-6 py-4 flex-row" >
       <Sidebar/>
       <BarChart/>
     </main>
