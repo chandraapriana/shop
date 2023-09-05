0a124f9b5fb88b0ee68cb70f7569b13183de1b51
chandraapriana <aprianachandra@gmail.com>
2023-09-05 10:59:32 +0700

    fix: refactor structure folder

---
+++ src/modules/CartPage/index.tsx
@@ -0,0 +1,11 @@
+import Sidebar from '@/components/Sidebar'
+
+const CartPage = () => {
+  return (
+    <main className="flex min-h-screen">
+      <Sidebar/>
+    </main>
+  )
+}
+
+export default CartPage
