68959b22bf33c8e49dc94272c3e0e37b47080026
chandraapriana <aprianachandra@gmail.com>
2023-09-07 00:11:46 +0700

    fix: remove server side

--- src/app/carts/[id]/page.tsx
+++ src/app/carts/[id]/page.tsx
@@ -4,9 +4,3 @@ import { NextPageContext } from "next";
 export default function DetailCart({ params }: { params: { id: string } }) {
   return <DetailCartPage id={params.id} />;
 }
-
-export async function getServerSideProps(context: NextPageContext) {
-  const { id } = context.query;
-
-  return { props: { id } };
-}
