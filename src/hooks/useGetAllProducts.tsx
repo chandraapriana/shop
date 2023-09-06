96af98f8b7015867eb09ed8d60717d23ee97c67f
chandraapriana <aprianachandra@gmail.com>
2023-09-06 16:45:45 +0700

    feat: add product table

--- src/hooks/useGetAllProducts.tsx
+++ src/hooks/useGetAllProducts.tsx
@@ -3,19 +3,19 @@ import { ProductType } from "@/utils/type/typeProduct";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const useGetAllProducts = () => {
+const useGetAllProducts = (page?: number) => {
   const [data, setData] = useState<ProductType[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   useEffect(() => {
     axios
-      .get("https://dummyjson.com/products")
+      .get(`https://dummyjson.com/products${page ? `?limit=${page * 10}` : ""}`)
       .then((res) => {
         setData(res.data.products);
         setIsLoading(true);
       })
       .catch((e) => console.log(e))
       .finally(() => setIsLoading(false));
-  }, []);
+  }, [page]);
 
   return { data, isLoading };
 };
