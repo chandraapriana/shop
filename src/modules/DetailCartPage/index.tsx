0e175bcc515d84a45ecd6994dd9309039767698c
chandraapriana <aprianachandra@gmail.com>
2023-09-06 23:46:54 +0700

    fix: state sidebar in detail cart page

--- src/modules/DetailCartPage/index.tsx
+++ src/modules/DetailCartPage/index.tsx
@@ -4,15 +4,16 @@ import "@/styles/globals.css";
 import Header from "@/components/Header";
 import CartCard from "./components/CartCard";
 import useGetDetailCart from "@/hooks/useGetDetailCart";
+import { useState } from "react";
 
 const DetailCartPage = ({ id }: { id: string }) => {
   const { data, user } = useGetDetailCart(id);
-
+  const [sidebar, setSidebar] = useState(false);
   return (
-    <main className="flex h-screen w-full px-6 py-4 flex-row">
-      <Sidebar />
+    <main className="flex h-screen w-full px-6 py-4 flex-row ">
+      <Sidebar showSidebar={sidebar} />
       <div className="w-full h-14">
-        <Header title="Cart Detail" />
+        <Header setSidebar={setSidebar} title="Cart Detail" />
         <CartCard data={data} user={user} />
       </div>
     </main>
