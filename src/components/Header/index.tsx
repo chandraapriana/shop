4f4d0ccad85e51fb684042b959fbc7e467c4644a
chandraapriana <aprianachandra@gmail.com>
2023-09-06 22:59:03 +0700

    fix: add font bold and text size

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 const Header = ({ title }: { title: string }) => {
   return (
     <div className="w-full h-10 mb-4 border-gray-400 border-b-[1px] flex justify-between items-center">
-      <h1>{title}</h1>
+      <h1 className="font-bold text-xl">{title}</h1>
       <div className="w-7 h-7 rounded-full bg-gray-500"></div>
     </div>
   );
