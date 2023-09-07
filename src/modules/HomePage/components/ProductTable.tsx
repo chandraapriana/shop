7d0e61a55d9fc31a437b0adbe488e13e6bea2fb2
chandraapriana <aprianachandra@gmail.com>
2023-09-07 10:21:13 +0700

    feat: add filter column

--- src/modules/HomePage/components/ProductTable.tsx
+++ src/modules/HomePage/components/ProductTable.tsx
@@ -8,17 +8,71 @@ import {
   getFilteredRowModel,
   useReactTable,
   getPaginationRowModel,
+  ColumnFiltersState,
+  FilterFn,
+  SortingFn,
+  sortingFns,
+  getSortedRowModel,
+  getFacetedRowModel,
+  getFacetedUniqueValues,
+  getFacetedMinMaxValues,
+  Column,
+  Table,
 } from "@tanstack/react-table";
 import { InputHTMLAttributes, useEffect, useMemo, useState } from "react";
+import {
+  RankingInfo,
+  rankItem,
+  compareItems,
+} from "@tanstack/match-sorter-utils";
+
+declare module "@tanstack/table-core" {
+  interface FilterFns {
+    fuzzy: FilterFn<unknown>;
+  }
+  interface FilterMeta {
+    itemRank: RankingInfo;
+  }
+}
+
+const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
+  // Rank the item
+  const itemRank = rankItem(row.getValue(columnId), value);
+
+  // Store the itemRank info
+  addMeta({
+    itemRank,
+  });
+
+  // Return if the item should be filtered in/out
+  return itemRank.passed;
+};
+
+const fuzzySort: SortingFn<any> = (rowA, rowB, columnId) => {
+  let dir = 0;
+
+  // Only sort by rank if the column has ranking information
+  if (rowA.columnFiltersMeta[columnId]) {
+    dir = compareItems(
+      rowA.columnFiltersMeta[columnId]?.itemRank!,
+      rowB.columnFiltersMeta[columnId]?.itemRank!
+    );
+  }
+
+  // Provide an alphanumeric fallback for when the item ranks are equal
+  return dir === 0 ? sortingFns.alphanumeric(rowA, rowB, columnId) : dir;
+};
 
 const ProductTable = () => {
   const { data: dataProduct } = useGetAllProducts();
-  const [data, setData] = useState(() => [...dataProduct]);
-  const [globalFilter, setGlobalFilter] = useState("");
-
+  const [data, setdata] = useState([...dataProduct]);
   useEffect(() => {
-    setData(dataProduct);
+    setdata(dataProduct);
+    return () => {};
   }, [dataProduct]);
+
+  const [globalFilter, setGlobalFilter] = useState("");
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
 
   const columnHelper = createColumnHelper<ProductType>();
 
@@ -52,62 +106,91 @@ const ProductTable = () => {
   const table = useReactTable({
     data,
     columns,
-    state: {
-      globalFilter: globalFilter,
+    filterFns: {
+      fuzzy: fuzzyFilter,
     },
+    state: {
+      columnFilters,
+      globalFilter,
+    },
+    onColumnFiltersChange: setColumnFilters,
     onGlobalFilterChange: setGlobalFilter,
+    globalFilterFn: fuzzyFilter,
     getCoreRowModel: getCoreRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    getFacetedRowModel: getFacetedRowModel(),
+    getFacetedUniqueValues: getFacetedUniqueValues(),
+    getFacetedMinMaxValues: getFacetedMinMaxValues(),
+    debugTable: true,
+    debugHeaders: true,
+    debugColumns: false,
   });
 
   return (
-    <div className=" flex flex-col mt-4 justify-between bg-white p-2 overflow-x-auto rounded-2xl shadow-xl w-full max-w-[800px]  min-h-[556px] h-auto lg:h-[556px]">
-      <div className="flex flex-row justify-between">
-        <h1 className="font-bold">Table Product</h1>
-        <DebouncedInput
-          value={globalFilter ?? ""}
-          onChange={(value) => setGlobalFilter(String(value))}
-          className="p-2 font-lg shadow border border-block rounded-lg"
-          placeholder="Search all columns..."
-        />
+    <div className=" flex flex-col mt-4 justify-between bg-white p-2 overflow-x-auto rounded-2xl shadow-xl w-full max-w-[1000px]  min-h-[556px] h-auto lg:h-auto">
+      <div>
+        <div className="flex flex-row justify-between">
+          <h1 className="font-bold">Table Product</h1>
+          <DebouncedInput
+            value={globalFilter ?? ""}
+            onChange={(value) => setGlobalFilter(String(value))}
+            className="p-2 font-lg shadow border border-block rounded-lg"
+            placeholder="Search all columns..."
+          />
+        </div>
+        <div className="p-2   h-[90%]">
+          <table className="w-full  !h-[80px]">
+            {/* Table Header */}
+            <thead className=" bg-[#F9FAFC] h-10 rounded-2xl">
+              {table.getHeaderGroups().map((headerGroup) => (
+                <tr key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <th className={`text-center px-5`} key={header.id}>
+                        {header.isPlaceholder ? null : (
+                          <>
+                            {flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                            {header.column.getCanFilter() &&
+                            (header.id === "title" ||
+                              header.id === "brand" ||
+                              header.id === "price" ||
+                              header.id === "category") ? (
+                              <div>
+                                <Filter column={header.column} table={table} />
+                              </div>
+                            ) : null}
+                          </>
+                        )}
+                      </th>
+                    );
+                  })}
+                </tr>
+              ))}
+            </thead>
+            {/* Table Body */}
+            <tbody className="h-auto  mt-10">
+              {table.getRowModel().rows.map((row) => (
+                <tr className="" key={row.id}>
+                  {row.getVisibleCells().map((cell) => (
+                    <td className={"text-center  py-2"} key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
 
-      <div className="p-2   h-[90%]">
-        <table className="w-full  !h-[80px]">
-          {/* Table Header */}
-          <thead className=" bg-[#F9FAFC] h-10 rounded-2xl">
-            {table.getHeaderGroups().map((headerGroup) => (
-              <tr className="pl-5" key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <th className={`text-center`} key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </th>
-                  );
-                })}
-              </tr>
-            ))}
-          </thead>
-          {/* Table Body */}
-          <tbody className="h-auto  mt-10">
-            {table.getRowModel().rows.map((row) => (
-              <tr className="" key={row.id}>
-                {row.getVisibleCells().map((cell) => (
-                  <td className={"text-center  py-2"} key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
       <div className="flex  justify-end">
         <div className="bg-white p-2 rounded-2xl shadow-xl gap-3">
           <button
@@ -144,10 +227,86 @@ const ProductTable = () => {
 
 export default ProductTable;
 
+function Filter({
+  column,
+  table,
+}: {
+  column: Column<any, unknown>;
+  table: Table<any>;
+}) {
+  const firstValue = table
+    .getPreFilteredRowModel()
+    .flatRows[0]?.getValue(column.id);
+
+  const columnFilterValue = column.getFilterValue();
+
+  const sortedUniqueValues = useMemo(
+    () =>
+      typeof firstValue === "number"
+        ? []
+        : Array.from(column.getFacetedUniqueValues().keys()).sort(),
+    [column.getFacetedUniqueValues()] //eslint-disable-line
+  );
+
+  return typeof firstValue === "number" ? (
+    <div>
+      <div className="flex space-x-2">
+        <DebouncedInput
+          type="number"
+          min={Number(column.getFacetedMinMaxValues()?.[0] ?? "")}
+          max={Number(column.getFacetedMinMaxValues()?.[1] ?? "")}
+          value={(columnFilterValue as [number, number])?.[0] ?? ""}
+          onChange={(value) =>
+            column.setFilterValue((old: [number, number]) => [value, old?.[1]])
+          }
+          placeholder={`Min ${
+            column.getFacetedMinMaxValues()?.[0]
+              ? `(${column.getFacetedMinMaxValues()?.[0]})`
+              : ""
+          }`}
+          className="w-24 border shadow rounded"
+        />
+        <DebouncedInput
+          type="number"
+          min={Number(column.getFacetedMinMaxValues()?.[0] ?? "")}
+          max={Number(column.getFacetedMinMaxValues()?.[1] ?? "")}
+          value={(columnFilterValue as [number, number])?.[1] ?? ""}
+          onChange={(value) =>
+            column.setFilterValue((old: [number, number]) => [old?.[0], value])
+          }
+          placeholder={`Max ${
+            column.getFacetedMinMaxValues()?.[1]
+              ? `(${column.getFacetedMinMaxValues()?.[1]})`
+              : ""
+          }`}
+          className="w-24 border shadow rounded"
+        />
+      </div>
+      <div className="h-1" />
+    </div>
+  ) : (
+    <>
+      <datalist id={column.id + "list"}>
+        {sortedUniqueValues.slice(0, 5000).map((value: any) => (
+          <option value={value} key={value} />
+        ))}
+      </datalist>
+      <DebouncedInput
+        type="text"
+        value={(columnFilterValue ?? "") as string}
+        onChange={(value) => column.setFilterValue(value)}
+        placeholder={`Search... (${column.getFacetedUniqueValues().size})`}
+        className="w-36 border shadow rounded"
+        list={column.id + "list"}
+      />
+      <div className="h-1" />
+    </>
+  );
+}
 function DebouncedInput({
   value: initialValue,
   onChange,
-  debounce = 200,
+  debounce = 500,
   ...props
 }: {
   value: string | number;
@@ -166,7 +325,7 @@ function DebouncedInput({
     }, debounce);
 
     return () => clearTimeout(timeout);
-  }, [value, debounce, onChange]);
+  }, [value]); //eslint-disable-line
 
   return (
     <input
